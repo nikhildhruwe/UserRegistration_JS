c01fdb683dbf9bff0278087eb1fd9ee026e76463
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-03 01:32:19 +0530

    Added: Code to check  first name using regex pattern

--- userRegistration.js
+++ userRegistration.js
@@ -1,1 +1,10 @@
 console.log("\tWelcome to user registration");
+
+let prompt = require('prompt-sync')();
+let firstNamePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+
+let name = prompt("Enter First Name: ");
+if(firstNamePattern.test(name))
+    console.log("saved.");
+else
+    console.log("Enter Valid Name");
