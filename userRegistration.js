30d2e5eecc91dcbf1fa13baa7e4d306cf11f1ebc
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-03 03:03:19 +0530

    Added: Code to check mobile number using regex pattern

--- userRegistration.js
+++ userRegistration.js
@@ -3,7 +3,7 @@ console.log("\tWelcome to user registration");
 let prompt = require('prompt-sync')();
 let namePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
 let emailPattern = new RegExp("^[a-zA-Z0-9]+([.+-_]?[a-zA-Z0-9]+)*@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$");
-
+let mobilePAttern = new RegExp("[0-9]{2}[ |-]?[0-9]{10}$");
 //First Name Validation.
 while(true){
     let name = prompt("Enter First Name: ");
@@ -31,6 +31,17 @@ while(true){
     let email = prompt("Enter Email ID: ");
     if(!emailPattern.test(email))
         console.log("Invalid input.");
+    else{
+        console.log("Saved.\n");
+           break;  
+        }
+}
+
+//Mobile Validation
+while(true){
+    let mobile = prompt("Enter mobile number : ");
+    if(!mobilePAttern.test(mobile))
+        console.log("Invalid input, example : 91 9876543210");
     else{
         console.log("Saved.\n");
            break;  
