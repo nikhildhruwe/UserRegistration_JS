894a846cf10a7f35d8c178e8141a6733997d06e8
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-03 02:28:22 +0530

    Added: Code to check last name using regex pattern.

--- userRegistration.js
+++ userRegistration.js
@@ -2,9 +2,26 @@ console.log("\tWelcome to user registration");
 
 let prompt = require('prompt-sync')();
 let firstNamePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+let lastNamePattern = new RegExp("^[A-Z][a-z]{2,}$");
 
-let name = prompt("Enter First Name: ");
-if(firstNamePattern.test(name))
-    console.log("saved.");
-else
-    console.log("Enter Valid Name");
+//First Name Validation.
+while(true){
+    let name = prompt("Enter First Name: ");
+    if(!firstNamePattern.test(name))
+        console.log("Invalid, give proper name.");
+    else{
+    console.log("Saved.\n");
+    break;
+    }
+}
+
+//Last Name Validation.
+while(true){
+    let name = prompt("Enter Last Name: ");
+    if(!lastNamePattern.test(name))
+        console.log("Invalid, give proper name.");
+    else{
+        console.log("Saved.\n");
+           break;  
+        }
+}
