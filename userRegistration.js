6aaf31b68a64920e39a374c9cb38cf715fea89ab
nikhildhruwe <nick.dhruwe@gmail.com>
2020-08-03 02:44:20 +0530

    Added: Code to check email pattern

--- userRegistration.js
+++ userRegistration.js
@@ -1,13 +1,13 @@
 console.log("\tWelcome to user registration");
 
 let prompt = require('prompt-sync')();
-let firstNamePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
-let lastNamePattern = new RegExp("^[A-Z][a-z]{2,}$");
+let namePattern = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+let emailPattern = new RegExp("^[a-zA-Z0-9]+([.+-_]?[a-zA-Z0-9]+)*@([0-9a-zA-Z][-]?)+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$");
 
 //First Name Validation.
 while(true){
     let name = prompt("Enter First Name: ");
-    if(!firstNamePattern.test(name))
+    if(!namePattern.test(name))
         console.log("Invalid, give proper name.");
     else{
     console.log("Saved.\n");
@@ -18,8 +18,19 @@ while(true){
 //Last Name Validation.
 while(true){
     let name = prompt("Enter Last Name: ");
-    if(!lastNamePattern.test(name))
+    if(!namePattern.test(name))
         console.log("Invalid, give proper name.");
+    else{
+        console.log("Saved.\n");
+           break;  
+        }
+}
+
+//Email Validation
+while(true){
+    let email = prompt("Enter Email ID: ");
+    if(!emailPattern.test(email))
+        console.log("Invalid input.");
     else{
         console.log("Saved.\n");
            break;  
